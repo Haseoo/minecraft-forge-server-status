1fdebf8d9a953e86ccba81561b77d125f335a19a
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2020-08-28 19:21:51 +0200

    Spring initializr

---
+++ src/main/java/com/github/haseoo/minecraft/statusapi/StatusapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.haseoo.minecraft.statusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/haseoo/minecraft/statusapi/StatusapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.haseoo.minecraft.statusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
